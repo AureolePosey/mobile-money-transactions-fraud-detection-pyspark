f3550c4171751f7f48e79b064c195b7591f1a0f0
Aureole Rogelio Edjekpoto <edjekpotorogelio7@gmail.com>
2026-03-14 14:30:33 +0100

    feat: add rule-based fraud detection logic

---
+++ fraud-detection-pipeline/pipeline/fraud_detection.py
@@ -0,0 +1,69 @@
+from utils.spark_session import create_spark_session
+from pyspark.sql.functions import col, when
+
+
+def detect_fraud():
+
+    spark = create_spark_session()
+
+    print("Loading features dataset...")
+
+    df = spark.read.parquet("data/curated/transactions_features")
+
+    # --------------------------------
+    # Rule 1 : very high transaction
+    # --------------------------------
+
+    df = df.withColumn(
+        "rule_high_amount",
+        when(col("amount") > 1000000, 1).otherwise(0)
+    )
+
+    # --------------------------------
+    # Rule 2 : too many transactions
+    # --------------------------------
+
+    df = df.withColumn(
+        "rule_too_many_transactions",
+        when(col("transactions_per_user_per_day") > 10, 1).otherwise(0)
+    )
+
+    # --------------------------------
+    # Rule 3 : abnormal amount
+    # --------------------------------
+
+    df = df.withColumn(
+        "rule_abnormal_amount",
+        when(col("amount") > (col("avg_transaction_amount_per_user") * 5), 1)
+    )
+
+    # --------------------------------
+    # Final fraud flag
+    # --------------------------------
+
+    df = df.withColumn(
+        "fraud_flag",
+        when(
+            (col("rule_high_amount") == 1) |
+            (col("rule_too_many_transactions") == 1) |
+            (col("rule_abnormal_amount") == 1),
+            1
+        ).otherwise(0)
+    )
+
+    print("Fraud detection completed")
+
+    df.show(10)
+
+    return df
+
+
+if __name__ == "__main__":
+
+    df_fraud = detect_fraud()
+
+    df_fraud.write \
+        .mode("overwrite") \
+        .parquet("data/analytics/fraud_transactions")
+
+    print("Fraud dataset saved")
